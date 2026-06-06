808800d939319dca3263d8a78e2c5e12b38af951
R. Meier <remad@web.de>
2026-06-06 13:15:56 +0200

    master: Producer Consumer with Condition Variables

--- Producer-Consumer-with-Condition-Variables/src/main/java/edu/remad/Main.java
+++ Producer-Consumer-with-Condition-Variables/src/main/java/edu/remad/Main.java
@@ -1,13 +1,28 @@
 package edu.remad;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Lock lock = new ReentrantLock();
 		
-		lock.newCondition();
+		Condition condition = lock.newCondition();
+		
+		// Thread1
+		lock.lock();
+		
+		condition.await();
+		
+		lock.unlock();
+		
+		// Thread 2
+		lock.lock();
+		
+		condition.signal();
+		
+		lock.unlock();
 	}
 }
