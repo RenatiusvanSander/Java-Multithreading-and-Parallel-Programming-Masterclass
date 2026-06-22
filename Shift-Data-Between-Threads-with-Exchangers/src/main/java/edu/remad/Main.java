702a7bb0f768f766fc03b5639f9a59dff6d3a351
Remy Meier <remad@web.de>
2026-06-22 23:09:45 +0200

    master: Shift Data Between Threads with Exchangers

---
+++ Shift-Data-Between-Threads-with-Exchangers/src/main/java/edu/remad/Main.java
@@ -0,0 +1,23 @@
+package edu.remad;
+
+import java.util.concurrent.Exchanger;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		Exchanger<String> exchanger = new Exchanger<>();
+		
+		Thread t = new Thread(() -> {
+			try {
+				String receivedValue = exchanger.exchange("value1");
+				System.out.println("Received: " + receivedValue + " in thread " + Thread.currentThread());
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		t.start();
+		
+		String receivedValue = exchanger.exchange("value2");
+		System.out.println("Received: " + receivedValue + " in thread " + Thread.currentThread().getName());
+	}
+}
