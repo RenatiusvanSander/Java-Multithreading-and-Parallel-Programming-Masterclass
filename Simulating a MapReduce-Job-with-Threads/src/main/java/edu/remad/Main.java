85c37e85ee21876599323ab56af5da8d5ac8cf24
R. Meier <remad@web.de>
2026-06-27 14:31:20 +0200

    master: Simulating a MapReduce Job with Threads - Part 2

--- Simulating a MapReduce-Job-with-Threads/src/main/java/edu/remad/Main.java
+++ Simulating a MapReduce-Job-with-Threads/src/main/java/edu/remad/Main.java
@@ -5,50 +5,96 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
 
 public class Main {
-	
+
 	private static final String input = "a friend in need is a friend indeed";
 
-	private static final List<Map.Entry<String, Integer>> intermediateResult = Collections.synchronizedList(new ArrayList<>());
-	
-	public static void main(String[] args) {
+	private static final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+	private static final List<Map.Entry<String, Integer>> intermediateResult = Collections
+			.synchronizedList(new ArrayList<>());
+	private static final List<List<Map.Entry<String, Integer>>> reducersInput = Collections
+			.synchronizedList(new ArrayList<>());
+
+	public static void main(String[] args) throws InterruptedException {
 		List<String> inputList = Arrays.asList(input.split(" "));
-		
+
 		new Thread(new Mapper(inputList.subList(0, inputList.size() / 2))).start();
 		new Thread(new Mapper(inputList.subList(inputList.size() / 2, inputList.size()))).start();
+
+		Thread partitioner = new Thread(new Partitioner());
+		partitioner.start();
+		partitioner.join();
+		
+		for(List<Map.Entry<String, Integer>> reducerInput: reducersInput) {
+			new Thread(new Reducer(reducerInput)).start();
+		}
+		
+		System.out.println("Test");
 	}
-	
+
 	static class Mapper implements Runnable {
 
 		private final List<String> input;
-		
+
 		public Mapper(List<String> input) {
 			this.input = input;
 		}
-		
+
 		@Override
 		public void run() {
-			for(String word: input) {
+			for (String word : input) {
 				intermediateResult.add(Map.entry(word, 1));
 			}
+
+			countDownLatch.countDown();
 		}
-		
+
 	}
-	
+
 	static class Partitioner implements Runnable {
 
 		@Override
 		public void run() {
+
+			try {
+				countDownLatch.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			List<String> uniqueWords = intermediateResult.stream().map(Map.Entry::getKey).distinct()
+					.collect(Collectors.toList());
+
+			for (String word : uniqueWords) {
+				List<Map.Entry<String, Integer>> reduceInput = intermediateResult.stream()
+						.filter(entry -> entry.getKey().equals(word)).collect(Collectors.toList());
+
+				reducersInput.add(reduceInput);
+			}
 		}
-		
+
 	}
-	
+
 	static class Reducer implements Runnable {
+		
+		private final List<Map.Entry<String, Integer>> reducerInput;
+		
+		public Reducer(List<Map.Entry<String, Integer>> reducerInput) {
+			this.reducerInput = reducerInput;
+		}
 
 		@Override
 		public void run() {
+			int S =0;
+			for(Map.Entry<String, Integer> entry: reducerInput) {
+				S += entry.getValue();
+			}
+			System.out.println("The word " + reducerInput.get(0).getKey() + " => occurrences: " + S);
 		}
-		
+
 	}
 }
