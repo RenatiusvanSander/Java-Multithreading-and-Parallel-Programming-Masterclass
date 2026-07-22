f355c0d15f4d7cf1553ffd25b171e5af34f9e56b
R. Meier <remad@web.de>
2026-07-22 16:07:50 +0200

    master: scheduling part 2

--- Monitoring-the-Performance-of-a-ThreadPool/src/main/java/edu/remad/Main.java
+++ Monitoring-the-Performance-of-a-ThreadPool/src/main/java/edu/remad/Main.java
@@ -1,8 +1,21 @@
 package edu.remad;
 
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		ScheduledThreadPoolExecutor threadPoolExecutor = new ScheduledThreadPoolExecutor(3);
+		threadPoolExecutor.setRemoveOnCancelPolicy(true);
+		threadPoolExecutor.setExecuteExistingDelayedTasksAfterShutdownPolicy(true);
+		threadPoolExecutor.setContinueExistingPeriodicTasksAfterShutdownPolicy(true);
+		
+		ScheduledFuture<?> future = threadPoolExecutor.scheduleAtFixedRate(() -> System.out.println("Task"), 5, 1, TimeUnit.SECONDS);
+		future.cancel(true);
+		
+		threadPoolExecutor.shutdown();
 		
 	}
 
