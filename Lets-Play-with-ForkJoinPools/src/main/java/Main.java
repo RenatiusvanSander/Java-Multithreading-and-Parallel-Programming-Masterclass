3dd491c9c09d1b0c4eb33259d2a8f42c960a20d9
Remy Meier <remad@web.de>
2026-07-26 13:29:48 +0200

    master: Let's Play with ForkJoinPools

---
+++ Lets-Play-with-ForkJoinPools/src/main/java/Main.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveAction;
+
+public class Main {
+
+	private static int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ForkJoinPool pool = new ForkJoinPool(2);
+
+		Future<?> future = pool.submit(new IncrementTask(0, 8));
+		future.get();
+		
+		System.out.println("The array is: " + Arrays.toString(array));
+	}
+	
+	static class IncrementTask extends RecursiveAction {
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 8499875297048073315L;
+		
+		private final int left;
+		private final int right;
+		
+		public IncrementTask(int left, int right) {
+			this.left = left;
+			this.right = right;
+		}
+
+		@Override
+		protected void compute() {
+			if(right - left < 3) {
+				for(int i = left; i < right; i++) {
+					array[i]++;
+				}
+			} else {
+				int mid = (left + right) / 2;
+				invokeAll(new IncrementTask(left, mid),new IncrementTask(mid, right));
+			}
+		}
+	}
+}
