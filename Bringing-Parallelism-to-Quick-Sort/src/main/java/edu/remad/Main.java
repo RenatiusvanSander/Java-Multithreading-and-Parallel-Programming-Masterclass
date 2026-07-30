9e406f85478ef4843cbd8477f93a55d917bd9c71
Remy Meier <remad@web.de>
2026-07-30 20:05:28 +0200

    master: quicksort

---
+++ Bringing-Parallelism-to-Quick-Sort/src/main/java/edu/remad/Main.java
@@ -0,0 +1,91 @@
+package edu.remad;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+
+public class Main {
+
+	private static Random random = new Random();
+	private static int arraySize = 100000000;
+	private static int[] array = new int[arraySize];
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = random.nextInt(arraySize);
+		}
+
+		System.out.println(Arrays.toString(array));
+
+//		quickSort(0, arraySize - 1);
+		
+		long start = System.nanoTime();
+
+		ForkJoinPool forkJoinPool = new ForkJoinPool();
+		ForkJoinTask<Void> future = forkJoinPool.submit(new QuickSortTask(0, arraySize - 1));
+		future.get();
+		
+		long end = System.nanoTime();
+		
+		System.out.println("Execution time = " + (end - start));
+
+		System.out.println(Arrays.toString(array));
+	}
+
+	static class QuickSortTask extends RecursiveAction {
+
+		private final int left;
+		private final int right;
+
+		public QuickSortTask(int left, int right) {
+			this.left = left;
+			this.right = right;
+		}
+
+		@Override
+		protected void compute() {
+			if (left < right) {
+				int pivotIndex = partition(left, right);
+
+				invokeAll(new QuickSortTask(left, pivotIndex - 1), new QuickSortTask(pivotIndex + 1, right));
+			}
+		}
+
+	}
+
+	static int partition(int left, int right) {
+		int pivot = array[right];
+		int swapIndex = left - 1;
+
+		for (int i = left; i < right; i++) {
+			if (array[i] < pivot) {
+				swapIndex++;
+				swap(swapIndex, i);
+			}
+		}
+
+		swapIndex++;
+		swap(swapIndex, right);
+
+		return swapIndex;
+	}
+
+	static void quickSort(int left, int right) {
+		if (left < right) {
+			int pivotIndex = partition(left, right);
+
+			quickSort(left, pivotIndex - 1);
+			quickSort(pivotIndex + 1, right);
+		}
+	}
+
+	static void swap(int leftIndex, int rightIndex) {
+		int aux = array[leftIndex];
+		array[leftIndex] = array[rightIndex];
+		array[rightIndex] = aux;
+	}
+
+}
